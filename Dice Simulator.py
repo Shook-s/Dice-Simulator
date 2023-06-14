6ac7281f6ac27710f4bfb04f34fb8085c760b576
Shook-s <109426852+Shook-s@users.noreply.github.com>
2023-06-14 18:20:44 +0300

    Dice Simulator prototype 1 working
    The initial dice simulation works but I want to add more features

--- Dice Simulator.py
+++ Dice Simulator.py
@@ -3,24 +3,17 @@
 import random
 
 print("hi is this is a dice simulator by shook-s")
-roll_yes_no = input("Do you want to roll the dice? Y/N\n")
 
 while True:
     roll_yes_no = input("Do you want to roll the dice? Y/N\n")
     if roll_yes_no in ["1","y","Y","2","n","N"]:
         if roll_yes_no in ["1","y","Y"]:
-            roll_yes_no = "Y"
+            rolling = random.randint(1, 6)
+            print("Rolling...")
+            print(f"You got {rolling}!")
             break
         else:
-            roll_yes_no = "N"
+            print("Bye bye")
             break
     else:
         print("You didn't pick yes or no")
-
-if roll_yes_no == "Y":
-    rolling = random.random(1,6)
-    print(rolling)
-
-
-
-# For the future I want to do multiple rolls and it would print said outputs
