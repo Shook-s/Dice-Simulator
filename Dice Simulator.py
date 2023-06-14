476db63d459855062b69b2c7f8b9b57b92c8efe4
Shook-s <109426852+Shook-s@users.noreply.github.com>
2023-06-14 19:19:47 +0300

    Dice Simulator is complete

--- Dice Simulator.py
+++ Dice Simulator.py
@@ -8,20 +8,33 @@ def roll():
     if roll_yes_no == "Y":
         while True:
             try:
-                number_of_die = int(input("How many die do you want to roll?\n"))
-                if int(number_of_die) == number_of_die:
+                number_of_die = int(input("How many dice do you want to roll?\n"))
+                if number_of_die > 0:
                     break
+                else:
+                    print("Enter a positive number.")
             except ValueError:
-                print("Error pick a number")
+                print("Error: Enter a valid number")
 
+        counts = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
         rolls = []
-        for x in range(number_of_die):
+
+        for _ in range(number_of_die):
             rolling = random.randint(1, 6)
             rolls.append(str(rolling))
+            if number_of_die > 10:
+                counts[rolling] += 1
 
         result = ", ".join(rolls)
         print(f"The dice rolls are: [{result}]")
+
+        if number_of_die > 10:
+            print("Since you rolled the die a lot..\nHere is what I found out!")
+            for number, count in counts.items():
+                print(f"{number}s: {count}")
+
         play_again()
+
 
 def play_again():    
     roll_again = input("Do you want to roll again? Y/N\n")
